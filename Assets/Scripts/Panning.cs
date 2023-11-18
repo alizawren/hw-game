b0478b450433806c256c51796d34679001e26079
maximtsai <maximtsai@yahoo.com>
2023-11-18 13:17:48 -0800

    Converting all to prefabs

--- Assets/Scripts/Panning.cs
+++ Assets/Scripts/Panning.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Panning : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private Vector3 positionCamera;
+    private Camera camera;
+
+    void Start () {
+        camera = GetComponent<Camera>();
+        positionCamera = camera.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePos = Input.mousePosition;
+        positionCamera = new Vector3((mousePos.x - 360) * 0.0035f, (mousePos.y - 320) * 0.0035f, -10f);
+        camera.transform.position = positionCamera;
     }
 }
