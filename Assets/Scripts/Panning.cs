5c048101055702abfdd6e8e962f583a033a44610
maximtsai <maximtsai@yahoo.com>
2023-11-19 22:31:23 -0800

    gameboy fixes

--- Assets/Scripts/Panning.cs
+++ Assets/Scripts/Panning.cs
@@ -19,6 +19,7 @@ public class Panning : MonoBehaviour
     void Update()
     {
         if (0.0f == Time.timeScale) return;
+        positionCamera = camera.transform.position;
         Vector3 mousePos = Input.mousePosition;
         float changeRatio = Mathf.Min(1.25f, Time.deltaTime / 0.00166f) * 0.01f + 0.002f;
         float goalX = (mousePos.x - 360) * panAmt;
